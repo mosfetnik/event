49074cb6e0bc13483dd2cfd4ea1a32158bf5502a
Aditya <89684511+mosfetnik@users.noreply.github.com>
2023-09-19 12:57:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hello world");
+let num = "michal";
+let number = 2;
+let any = true;
+
+console.log(typeof any);
+
+let a = [10, 20, 30, 40];
+let b = a;
+
+// b.pop();
+console.log(b);
+
+// let age = 19;
+// if (age>18) {
+//   console.log("Drive");
+
+// } else {
+//   console.log("Not Drive");
+// }
+
+age = 100;
+age > 100 ? console.log("Drive") : console.log("Not Drive");
+
+function counter() {
+  let num = 1;
+  num = num + 1;
+  console.log(num);
+}
+
+// for (var i = 0; i < 11; ++i) {
+//   console.log(i);
+// }
+
+// creating the function
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+// calling the function
+sum();
+console.log(sum(4, 5));
+const car = { 
+  type: "Fiat",
+   model: "500", 
+   color: "white"
+   };
